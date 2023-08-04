c2843d1c0428bb3895e60ee4b75ddb29879d8204
jieun <you@example.com>
2023-08-04 17:35:23 +0900

    root비밀번호 까먹었다..

--- Hi/db.js
+++ Hi/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'your_mysql_password',
-  database: 'your_database_name',
+  password: '1234',
+  database: 'data',
 });
 
 connection.connect((err) => {
