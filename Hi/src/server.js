7d25edba0ccf40ff8bfe540d01cf00534beeb611
jungjieun <je_eun2735@naver.com>
2023-08-05 18:31:00 +0900

    EJS 템플릿

--- Hi/src/server.js
+++ Hi/src/server.js
@@ -4,18 +4,24 @@ const app = express();
 const PORT = 3000;
 const db = require('../db');
 
-//전송받은 폼을 읽기위해 POST 요청 본문을 파싱하는 미들웨어
+// EJS 템플릿 엔진 설정
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// 전송받은 폼을 읽기위해 POST 요청 본문을 파싱하는 미들웨어
 app.use(express.urlencoded({ extended: false }));
+
 // 정적 파일을 제공하는 미들웨어 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// '/' 경로에 index.html 파일을 띄우기 위한 라우트 설정
+// '/' 경로에 index.ejs 파일을 띄우기 위한 라우트 설정
 app.get('/', (req, res) => {
-  db.query('SELECT * FROM posts', (err,results) => {
+  db.query('SELECT * FROM posts', (err, results) => {
     if (err) throw err;
-    res.send(results);
-    //모든 테이블의 데이터를 조회하고 결과를 보내기 위해 작성
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    // 조회한 결과를 res.locals에 저장
+    res.locals.posts = results;
+    // index.ejs 파일을 렌더링하여 클라이언트에 전송
+    res.render('index');
   });
 });
 
